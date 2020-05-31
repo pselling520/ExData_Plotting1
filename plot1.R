8326f17f12ba9d1bf59d8573cf0512667a6c1847
Paul Selling <pselling520@gmail.com>
2020-05-31 15:09:37 -0500

    Adding first exploratory plot

---
+++ plot1.R
@@ -0,0 +1,19 @@
+
+#Loading in data, recasting some columns
+data <- read.table(file,header=TRUE, sep=";")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data <- subset(data,((Date>="2007-02-01") & 
+                     (Date<="2007-02-02")))
+data$Time <- strptime(data$Time,"%H:%M:%S")
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+
+#Creating graph
+hist(data$Global_active_power, 
+     breaks=15,
+     col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+
+dev.copy(png, file="plot1.png")
+dev.off()
+
